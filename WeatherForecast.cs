4c190d57e6aa2acbad32a803eb0bea8c18b4baa5
NaouresBzeouich <naoures.bzeouich@insat.ucar.tn>
2023-12-05 22:01:15 +0100

    initializing the project

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Project_back_end
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
