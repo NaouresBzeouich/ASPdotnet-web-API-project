3b2e15d8bc43a8d0fbcd2f58b26d1a46f23917ad
NaouresBzeouich <naoures.bzeouich@insat.ucar.tn>
2023-12-07 21:31:08 +0100

    adding Blog class

---
+++ Models/Blog.cs
@@ -0,0 +1,30 @@
+﻿namespace Project_back_end.Models
+{
+    public class Blog
+    {
+        // blog Id
+        public int Id { get; set; }
+
+        // blog contains : title , text(Content) and image 
+        public string Content { get; set; }
+        public string Title { get; set; }
+        public string Image {  get; set; }
+
+        // the number of views in this blog
+        public int Views { get; set; }
+
+        // rate associated to the blog Somme(like : +1 ,dislike : -1)
+        public int rating { get; set; }
+
+        // association between Blog and its owner ( each bog has only one owner)
+        
+        
+
+        // association between Blog and comment : each blog can have 0..* comment(s)
+        
+        public Blog()
+        {
+
+        }
+    }
+}
