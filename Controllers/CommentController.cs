f6167e0b3e8d6128d75252aae913e8c23a09a2bb
Ben Achour Skander <benachour.skander1@gmail.com>
2023-12-15 19:51:31 +0100

    comment model

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project_back_end.Data;
+using Project_back_end.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Project_back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,16 @@
+namespace Project_back_end.Models
+{
+    public class Comment
+    {
+        public guid Id { get; set; }
+        public string? Content { get; set; }
+        public int? rating { get; set; }
+		public virtual User User { get; set; }
+		public int? UserId { get; set; }
+        public virtual Blog Blog { get; set; }
+        public int? BlogId { get; set; }
+
+
+
+	}
+}
