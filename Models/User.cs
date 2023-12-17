1fa29a473ca48d11a4bddf4f04bd7a24f92ee215
Zied <ziedmaghraoui58@gmail.com>
2023-12-17 10:59:57 +0100

    added user join date on registration

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,7 @@ namespace Project_back_end.Models
     public class User : IdentityUser
     {
         public string? Bio { get; set; }
+        public DateTimeOffset JoinDate { get; set; } = DateTimeOffset.Now;
         // User has blogs :
         [JsonIgnore]
         public virtual ICollection<Blog>? Blogs { get; set; }
